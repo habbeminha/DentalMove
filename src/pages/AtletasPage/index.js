71bfc4adb90e9f69c8169ca1baa6bfc6495074e7
habbeminha <lalahabbema@gmail.com>
2021-01-18 09:59:38 -0300

    começo de atletas, fazer navegação certa

---
+++ src/pages/AtletasPage/index.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+import { RadioButton } from 'react-native-paper';
+import StyledInput from '../../components/StyledInput';
+import MainContainer from '../../components/MainContainer';
+import StyledButton from '../../components/StyledButton';
+import StyledLink from '../../components/StyledLink';
+import Title from '../../components/Title';
+import { RadioContainer } from './styles';
+
+import { firebase } from '../../firebase/config'
+
+const AtletasPage = ({navigation}) => {
+    
+}
