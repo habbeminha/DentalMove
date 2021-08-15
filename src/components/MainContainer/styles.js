309567912e7859a3432b0929fe35d12935b8af41
igortaquary <igortaquary@gmail.com>
2021-08-15 16:49:24 -0300

    fix: home page container size

--- src/components/MainContainer/styles.js
+++ src/components/MainContainer/styles.js
@@ -1,19 +1,18 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
+    flex: 1;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     padding: 50px;
-    min-height: 100%;
     width: 100%;
     background-color: #5599FF;
     font-weight: bold;
-    overflow: scroll;
 `;
 
 export const ScrollView = styled.ScrollView`
-    min-height: 100%;
     background-color: #5599FF;
+    flex: 1;
 `;
